ab707b4f1ae7a2869e3fbc824cde7542cfc95d22
Anna Bardina <dyavolenok84@yandex.ru>
2022-10-20 23:20:41 +0500

    First steps

--- tests.py
+++ tests.py
@@ -20,5 +20,40 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_rating(self):     #Проверяем присвоился или нет рейтинг
+        collector = BooksCollector()
+
+        collector.add_new_book('Имя ветра')
+        assert collector.get_book_rating('Имя ветра') == 1
+
+    def test_set_book_rating_set_new_rating(self):  #Проверяем изменился ли рейтинг у книги
+        collector = BooksCollector()
+        collector.add_new_book('Имя ветра')
+
+        collector.set_book_rating('Имя ветра', 6)
+        assert collector.get_book_rating('Имя ветра') == 6
+
+    def test_set_book_rating_zero_rating(self):     #Негативный тест. На нулевой рейтинг (нижняя граница)
+        collector = BooksCollector()
+        collector.add_new_book('Имя ветра')
+
+        collector.set_book_rating('Имя ветра', 0)
+        assert collector.get_book_rating('Имя ветра') == 1
+
+    def test_set_book_rating_more_possible(self):   #Негативный тест. За верхней границей
+        collector = BooksCollector()
+        collector.add_new_book('Имя ветра')
+
+        collector.set_book_rating('Имя ветра', 12)
+        assert collector.get_book_rating('Имя ветра') == 1
+
+    def test_get_book_rating_get_book_rating(self): #Получить рейтинг по названию книги
+        collector = BooksCollector()
+
+        collector.add_new_book('Имя ветра')
+        assert collector.get_book_rating('Имя ветра') == 1
+
+    def test_get_book_rating_get_book_rating(self): #Негативный тест. Нет такой книги
+        collector = BooksCollector()
+
+        assert collector.get_book_rating('Имя ветра') == None
